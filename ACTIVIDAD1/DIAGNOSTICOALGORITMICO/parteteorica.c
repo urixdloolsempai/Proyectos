8494007b37a69428efe882c5755972cfde5a574b
urixdloolsempai <urielnarice@gmail.com>
2026-03-02 00:04:11 -0300

    Add theoretical part of algorithm diagnosis project

---
+++ ACTIVIDAD1/DIAGNOSTICOALGORITMICO/parteteorica.c
@@ -0,0 +1,27 @@
+📘 PROYECTO 1: DiagnosticoAlgoritmos
+📌 1. Parte Teórica
+1️⃣ ¿Qué es un algoritmo y para qué se usa?
+Un algoritmo es una secuencia ordenada y finita de pasos que permite resolver un problema.
+En programación se utiliza para definir la lógica que seguirá un programa para cumplir una tarea específica.
+2️⃣ ¿Qué es un lenguaje de programación? Ejemplos
+Un lenguaje de programación es un sistema de reglas y sintaxis que permite escribir instrucciones que la computadora puede interpretar y ejecutar.
+Ejemplos:
+Python
+C
+Java
+JavaScript
+3️⃣ ¿Qué es una variable?
+Una variable es un espacio en memoria que almacena un dato que puede cambiar durante la ejecución del programa.
+Ejemplo en C:
+
+int edad = 16;
+
+4️⃣ ¿Qué es un bucle?
+Un bucle (o ciclo) es una estructura que permite repetir un conjunto de instrucciones mientras se cumpla una condición.
+Ejemplo:
+for
+while
+do while
+5️⃣ Diferencia entre función y método
+Función: Bloque de código que realiza una tarea específica y puede devolver un valor.
+Método: Es una función que pertenece a una clase (programación orientada a objetos).
